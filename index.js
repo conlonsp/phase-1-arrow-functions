099ab8e696ba372ea4e2cbf7136044c00fe1de6b
samuelconlon <conlonsp@yahoo.com>
2024-12-03 16:51:36 -0500

    commit tree 07b86e8c2131a1ed3a3d2cb827c50c35e0235254

---
+++ index.js
@@ -0,0 +1,48 @@
+const divide = (x, y) => x / y
+
+const square = x => x * x
+
+const add = (x, y) => x + y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const divide = function () {
+//     return 2000 / 100;
+// }
+// const square = x => x ** 2;
+
+// const add = (y, z) => y + z;
